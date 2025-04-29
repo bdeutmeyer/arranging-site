8343bf589940e8acaa2d4737e96057ccbd8db472
beth deutmeyer <bethdeutmeyer@gmail.com>
2025-04-28 19:50:35 -0500

    took hyphen out of thankyou path

--- src/main.jsx
+++ src/main.jsx
@@ -39,7 +39,7 @@ const router = createBrowserRouter([
         element: <Contact />,
       },
       {
-        path: 'thank-you',
+        path: 'thankyou',
         element: <ThankYou />
       }
     ],
