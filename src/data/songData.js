c7a1862a5a39888cd4b69ce9e91b34c4dbb822eb
beth deutmeyer <bethdeutmeyer@gmail.com>
2025-05-22 20:31:54 -0500

    pared down song list to revoicing stuff only

--- src/data/songData.js
+++ src/data/songData.js
@@ -7,32 +7,32 @@ import sehnsuchtImg from "../assets/images/sehnsucht.png"
 // To add songs: -check "upcoming" boolean of the last song, print to pdf then use preview to export p. 1 as png, releaseDate format YYYY-MM-DD, category = treble/bass/mixed, accidentals: ♭♯♮𝄪𝄫
 
 const songData = [
-    {
-        id: 1,
-        pathExt: 'danza-ttbb',
-        imagePath: danzaImg,
-        imageAlt: 'Danza, danza',
-        purchaseLink: 'https://www.jwpepper.com/danza-danza-11625730-2547492/p',
-        purchaseSite: 'J.W. Pepper',
-        price: '$2.50',  
-        listenLink: 'https://youtu.be/7R6h6a1PyrU',
-        composerLast: 'Durante',
-        composerFirst: 'Francesco',
-        songTitle: 'Danza, danza',
-        songParts: 'TTBB',
-        category: 'bass',
-        accompaniment: 'pianoOnly',
-        accompDescr: ' and piano',
-        releaseDate: '2025-02-12',
-        range: {
-            tenor1: 'F3 - G♭4',
-            tenor2: 'D♭3 - F4',
-            bass1: 'B♭3 - C4',
-            bass2: 'F2 - B♭3'
-        },
-        upcoming: false
+    // {
+    //     id: 1,
+    //     pathExt: 'danza-ttbb',
+    //     imagePath: danzaImg,
+    //     imageAlt: 'Danza, danza',
+    //     purchaseLink: 'https://www.jwpepper.com/danza-danza-11625730-2547492/p',
+    //     purchaseSite: 'J.W. Pepper',
+    //     price: '$2.50',  
+    //     listenLink: 'https://youtu.be/7R6h6a1PyrU',
+    //     composerLast: 'Durante',
+    //     composerFirst: 'Francesco',
+    //     songTitle: 'Danza, danza',
+    //     songParts: 'TTBB',
+    //     category: 'bass',
+    //     accompaniment: 'pianoOnly',
+    //     accompDescr: ' and piano',
+    //     releaseDate: '2025-02-12',
+    //     range: {
+    //         tenor1: 'F3 - G♭4',
+    //         tenor2: 'D♭3 - F4',
+    //         bass1: 'B♭3 - C4',
+    //         bass2: 'F2 - B♭3'
+    //     },
+    //     upcoming: false
 
-    },
+    // },
     {
         id: 2,
         pathExt: 'sicut-ssaa',
@@ -108,56 +108,56 @@ const songData = [
         }, 
         upcoming: false
     },
-    {
-        id: 5,
-        pathExt: 'se-tu-m\'ami',
-        imagePath: seTuMamiImg,
-        imageAlt: 'Se tu m\'ami',
-        purchaseLink: '',
-        purchaseSite: '',
-        price: '',  
-        listenLink: '',
-        composerLast: 'Parisotti',
-        composerFirst: 'presumably by Alessandro',
-        songTitle: 'Se tu m\'ami',
-        songParts: 'SSA',
-        category: 'treble',
-        accompaniment: 'pianoOnly',
-        accompDescr: ' and piano',
-        releaseDate: '',
-        range: {
-            soprano1: '',
-            soprano2: '',
-            alto: '',
-        }, 
-        upcoming: true
-    },
     // {
     //     id: 5,
-    //     pathExt: 'sehnsucht',
-    //     imagePath: sehnsuchtImg,
-    //     imageAlt: 'Sehnsucht',
-    //     purchaseLink: 'https://www.youtube.com',
-    //     purchaseSite: 'J.W. Pepper',
+    //     pathExt: 'se-tu-m\'ami',
+    //     imagePath: seTuMamiImg,
+    //     imageAlt: 'Se tu m\'ami',
+    //     purchaseLink: '',
+    //     purchaseSite: '',
     //     price: '',  
     //     listenLink: '',
-    //     composerLast: 'Brahms',
-    //     composerFirst: 'Johannes',
-    //     songTitle: 'Sehnsucht',
-    //     songSubtitle: 'Op. 112, No. 1',
-    //     songParts: 'SSAA',
+    //     composerLast: 'Parisotti',
+    //     composerFirst: 'presumably by Alessandro',
+    //     songTitle: 'Se tu m\'ami',
+    //     songParts: 'SSA',
     //     category: 'treble',
     //     accompaniment: 'pianoOnly',
     //     accompDescr: ' and piano',
     //     releaseDate: '',
     //     range: {
-    //         soprano1: 'E4 - F♯5',
-    //         soprano2: 'A3 - F♯5',
-    //         alto1: 'G♯3 - C♯5',
-    //         alto2: 'F♯3 - B4'
+    //         soprano1: '',
+    //         soprano2: '',
+    //         alto: '',
     //     }, 
     //     upcoming: true
     // },
+    {
+        id: 5,
+        pathExt: 'sehnsucht',
+        imagePath: sehnsuchtImg,
+        imageAlt: 'Sehnsucht',
+        purchaseLink: 'https://www.youtube.com',
+        purchaseSite: 'J.W. Pepper',
+        price: '',  
+        listenLink: '',
+        composerLast: 'Brahms',
+        composerFirst: 'Johannes',
+        songTitle: 'Sehnsucht',
+        songSubtitle: 'Op. 112, No. 1',
+        songParts: 'SSAA',
+        category: 'treble',
+        accompaniment: 'pianoOnly',
+        accompDescr: ' and piano',
+        releaseDate: '',
+        range: {
+            soprano1: 'E4 - F♯5',
+            soprano2: 'A3 - F♯5',
+            alto1: 'G♯3 - C♯5',
+            alto2: 'F♯3 - B4'
+        }, 
+        upcoming: true
+    },
 ];
 
 export default songData;
